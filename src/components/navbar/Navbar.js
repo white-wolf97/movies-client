768e2fbf6caa3859226638343c9cb571fc62b204
adolfoCastelo1 <adolfocastelo99@gmail.com>
2022-01-30 18:06:12 -0300

    feat: added font awesome icons

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -19,31 +19,33 @@ export const Navbar = () => {
 	}
 	return (
 
-		<nav className="navbar navbar-expand-sm navbar-dark bg-black">
+		<nav className="navbar navbar-expand-sm navbar-dark bg-black navbar__nav">
 			<Link className="navbar-brand" to="/">
 				Movies App
 			</Link>
 
 			<div className="navbar-collapse">
-				<div className="navbar-nav">
-
+				<div className="navbar-nav ">
 					<NavLink
 						className="nav-item nav-link" activeClassName="active"
-						to="/favorites">Favorites
+						to="/favorites"><div className="navbar__inner-div"><i className="far fa-heart navbar__icon" />Favorites</div>
 					</NavLink>
-
-
 				</div>
 			</div>
 
 			<div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
 				<ul className="navbar-nav ml-auto">
 					<span className="nav-item nav-link text-info">{name}</span>
+
 					<button onClick={handleLogout} className="nav-item nav-link btn">
-						Logout
+						<div>
+							<i class="fas fa-sign-out-alt navbar__icon"></i>
+							Logout
+						</div>
 					</button>
 				</ul>
 			</div>
+
 		</nav>
 	);
 };
